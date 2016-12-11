f984ea457433dff5148e547f84481990b9ed0606
Peter Corke <peter.i.corke@gmail.com>
2016-12-11 13:26:40 +1000

    add ability to map an RVC foldername to full path

--- rvcpath.m
+++ rvcpath.m
@@ -2,7 +2,10 @@
 %
 % p = RVCPATH is the path of the top level folder for the installed RVC
 % tools.
-
+%
+% p = RVCPATH(FOLDER) is the full path of the specified FOLDER which is relative to the
+% installed RVC tools.
+%
 % Copyright (C) 1993-2014, by Peter I. Corke
 %
 % This file is part of The Robotics Toolbox for MATLAB (RTB).
@@ -21,5 +24,9 @@
 % along with RTB.  If not, see <http://www.gnu.org/licenses/>.
 %
 % http://www.petercorke.com
-function p = rvcpath()
+function p = rvcpath(a)
     p = fileparts( which('startup_rvc.m') );
+    
+    if nargin > 0
+        p = fullfile(p, a);
+    end
